978df4db8e24faafbb3c3fb393515ebe4c2a92a0
SharjeelJ <->
2018-06-04 06:11:51 -0600

    Updated main.s
    - Added code to preserve registers to abide to the APCS
    - Added in the readData function to get data from the SNES controller
    - Removed the decrepated function clockLoop
    - Finished up the function readSNES (only thing left is to return a list containing the status of all the buttons)
    - Adjusted the decrepated function idleLoop to be more useful by looping the function readSNES

--- Assignment 1/source/main.s
+++ Assignment 1/source/main.s
@@ -34,16 +34,26 @@ main:
     mov         r2, #0xb001                         // Passes in the pin mode as a parameter
     bl          initGPIO                            // Calls the function to set the GPIO pin's status
 
-// Function that is an infinite loop to prevent the program from automatically terminating
-idleLoop:
-    b           idleLoop                            // Calls itself to keep looping
+// Function that is run till it is terminated or branched out of
+loopedProgram:
+    bl          readSNES                            // Calls the function to read the SNES controller's input
+
+    // Calls the appropriate button's function depending on what was pressed
+    ldr         r1, =buttonsList                    // Loads the list containing locations for all the buttons
+    ldr         r0, [r1, r0, lsl #2]                // Shifts across the list to call the appropriate button's function (2 bytes shift since word aligned list)
+//    bleq        printf                              // Calls the function to print to the console
+
+    bl          loopedProgram                       // Calls itself to keep looping
 
 // Function to initialize the passed in GPIO pin in the passed in mode
 // PARAMS:
 // r1 = GPIO pin # (0-53 inclusive)
 // r2 = GPIO pin mode (input being 0xb000 and output being 0xb001)
 initGPIO:
-    // Stores the total number of bits each pin has
+    // Stores the existing variable registers to the stack to abide to the APCS
+    push        {r4, r5, r6, r7, fp, lr}            // Pushes the specified registers to the stack to preserve them
+
+    // Stores the number of bits each pin has
     mov         r4, #3                              // Bits per pin
 
     // Determines which function select register needs to be used as well as the passed in GPIO's pin offset
@@ -119,6 +129,8 @@ initGPIO:
         str     r5, [gpioBase, #0x4*5]              // Applies the changes to the specified GPIO pin by saving to memory
         b       endfunctionSelectTest               // Branches to the end of the function select register tests
     endfunctionSelectTest:
+        // Pops the stored existing variable registers from the stack to abide to the APCS
+        pop     {r4, r5, r6, r7, fp, lr}            // Pops the specified registers from the stack to preserve them
         bx      lr                                  // Branches back to the code that initially called the function
 
 // Function to set the value of GPIO pin 9 (Latch)
@@ -127,7 +139,7 @@ initGPIO:
 writeLatch:
     ldr         r2, =gpioPointer                    // Stores the pointer to the base GPIO address
     ldr         r2, [r2]                            // Updates the pointer address
-    mov         r3, #1                              // Stores the value that will be set to the GPIO pin
+    mov         r3, #1                              // Stores the value that will be used as a mask to narrow down to the appropriate pin
     lsl         r3, #9                              // Shifts the bit value to align it for the appropriate GPIO pin
     teq         r0, #0                              // Calls the appropriate operation (set / clear) depending on what was passed into the function
     streq       r3, [r2, #40]                       // Sets GPCLR0 to clear the GPIO pin's value
@@ -140,33 +152,38 @@ writeLatch:
 writeClock:
     ldr         r2, =gpioPointer                    // Stores the pointer to the base GPIO address
     ldr         r2, [r2]                            // Updates the pointer address
-    mov         r3, #1                              // Stores the value that will be set to the GPIO pin
+    mov         r3, #1                              // Stores the value that will be used as a mask to narrow down to the appropriate pin
     lsl         r3, #11                             // Shifts the bit value to align it for the appropriate GPIO pin
     teq         r0, #0                              // Calls the appropriate operation (set / clear) depending on what was passed into the function
     streq       r3, [r2, #40]                       // Sets GPCLR0 to clear the GPIO pin's value
     strne       r3, [r2, #28]                       // Sets GPSET0 to set the GPIO pin's value
     bx          lr                                  // Branches back to the code that initially called the function
 
+// Function to read from the value of GPIO pin 10 (Data)
+// RETURNS:
+// r0 = Value of GPIO pin 10 (Data)
 readData:
+    ldr         r2, =gpioPointer                    // Stores the pointer to the base GPIO address
+    ldr         r2, [r2]                            // Updates the pointer address
+    ldr         r1, [r2, #52]                       // Accesses GPLEV0 for data
+    mov         r3, #1                              // Stores the value that will be used to mask the GPIO pin data
+    lsl         r3, #10                             // Shifts the bit value to align it for the appropriate GPIO pin
+    and         r1, r3                              // Masks all the other bits
+    teq         r1, #0                              // Calls the appropriate operation (return 0 or 1) depending on what was returned from the GPIO pin
+    moveq       r0, #0                              // Returns 0
+    movne       r0, #1                              // Returns 1
     bx          lr                                  // Branches back to the code that initially called the function
 
-clockLoop:
-    // Delays for 6 microseconds
-    mov         r0, #6                              // Passes in the time in microseconds to delay the program for as a parameter
-    bl          delayMicroseconds                   // Calls the function to delay program progression
-    bx          lr                                  // Branches back to the code that initially called the function
-
-    // Sets GPIO pin 11 (Clock) to 0
-    mov         r0, #0                              // Passes in the value to set to the GPIO pin as a parameter
-    bl          writeClock                          // Calls the function to set the clock pin
-
-    // Delays for 6 microseconds
-    mov         r0, #6                              // Passes in the time in microseconds to delay the program for as a parameter
-    bl          delayMicroseconds                   // Calls the function to delay program progression
-
-    bx          lr                                  // Branches back to the code that initially called the function
-
+// Function to read from the SNES controller
+// RETURNS:
+// r0 = List containing the status of the buttons
 readSNES:
+    // Stores the existing variable registers to the stack to abide to the APCS
+    push        {r4, r5, fp, lr}                    // Pushes the specified registers to the stack to preserve them
+
+    // Register that will contain a list of the status of all the buttons
+    mov         r5, #0                              // Contains a list of the status of all the buttons and will be returned at the end
+
     // Sets GPIO pin 11 (Clock) to 1
     mov         r0, #1                              // Passes in the value to set to the GPIO pin as a parameter
     bl          writeClock                          // Calls the function to set the clock pin
@@ -183,6 +200,45 @@ readSNES:
     mov         r0, #0                              // Passes in the value to set to the GPIO pin as a parameter
     bl          writeLatch                          // Calls the function to set the latch pin
 
+    // Pulse loop to read the buttons from the SNES controller
+    mov         r4, #0                              // Initializes a loop iteration counter
+    readSNESLoop:
+        // Delays for 6 microseconds
+        mov     r0, #6                              // Passes in the value to set to the GPIO pin as a parameter
+        bl      delayMicroseconds                   // Calls the function to delay program progression
+
+        // Sets GPIO pin 11 (Clock) to 0
+        mov     r0, #0                              // Passes in the value to set to the GPIO pin as a parameter
+        bl      writeClock                          // Calls the function to set the clock pin
+
+        // Delays for 6 microseconds
+        mov     r0, #6                              // Passes in the value to set to the GPIO pin as a parameter
+        bl      delayMicroseconds                   // Calls the function to delay program progression
+
+        // Reads GPIO pin 10 (Data) at bit corresponding the loop counter
+        bl      readData                            // Calls the function to read the data pin
+        mov     r5, r0
+
+        // Sets GPIO pin 11 (Clock) to 1
+        mov     r0, #1                              // Passes in the value to set to the GPIO pin as a parameter
+        bl      writeClock                          // Calls the function to set the clock pin
+
+        // Calls the appropriate button's function depending on what was pressed
+        ldr     r1, =buttonsList                    // Loads the list containing locations for all the buttons
+        cmp     r5, #0                              // Checks to see if the current button was pressed
+        ldr     r0, [r1, r4, lsl #2]                // Shifts across the list to call the appropriate button's function (2 bytes shift since word aligned list)
+        bleq    printf                              // Calls the function to print to the console if the button was pressed
+
+        // Increments the loop iteration counter
+        add     r4, #1                              // Increments the loop iteration counter
+
+        // Keeps looping till all of the SNES controller buttons have been accounted for
+        cmp     r4, #16                             // Checks to see if there still are buttons that need to be checked
+        bllt    readSNESLoop                        // Loops back through the read loop to get the next button's status
+
+    // Pops the stored existing variable registers from the stack to abide to the APCS
+    mov         r0, r5
+    pop         {r4, r5, fp, lr}                    // Pops the specified registers from the stack to preserve them
     bx          lr                                  // Branches back to the code that initially called the function
 
 @ Data section
