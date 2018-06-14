3d7dd310de0380934f8b7c9c8947d359ac02d93d
bader-waseem <25441886+bader-waseem@users.noreply.github.com>
2018-06-14 00:38:38 -0600

    updated ball collisions and ball image data
    So the collisions should theoretically work, however everytime the ball is attempted to be drawn, there is a segmentation fault I can't figure out where it's coming from.

--- Assignment 2/source/ballMovement.s
+++ Assignment 2/source/ballMovement.s
@@ -1,44 +1,70 @@
+// PARAMS:
+// r0 = Image address of background
+// r1 = current X coordinate of ball
+// r2 = current Y coordinate of ball
+// We also need to pass in the direction, possibly through the stack
+
+// RETURNS:
+//  r0 = new X coordinate of ball
+//  r1 = new Y coordinate of ball
+//  We also need to return the direction, possible through the stack
+
 .global	ballMovement
 ballMovement:
-	// Storing registers to follow the APCS
-	push {r4 - r10, fp, lr}
-	
-	ldr	r0, =ballImage
-	mov	r1, #904
-	mov	r4, r1					// Copying X into r4
-	mov	r2, #516
-	mov	r5, r2					// Copying Y into r5
-	bl	drawImage
-	
-	
-	mov	r6, #800					// Left bound of the box
-	mov	r7, #1008					// Right bound of the box
-	
-left:
-	cmp	r4, r7
-	beq	right
-	mov	r0, #10000
-	bl	delayMicroseconds
-	add	r4, #1
-	ldr	r0, =ballImage
-	mov	r1, r4
-	mov	r2, #516
-	bl	drawImage
-	b	left
+    // Storing existing registers to abide by APCS
+    push    {r4 - r10, fp, lr}
+    
+    // Creating aliases for registers
+    xCoord              .req    r4
+    yCoord              .req    r5
+    backgroundWidth     .req    r6
+    backgroundHeight    .req    r7
+    directionX          .req    r8
+    directionY          .req    r9
+    move                .req    r10
+    
+    mov move, #1
+    
+    ldr backgroundWidth, [r0]
+    sub backgroundWidth, backgroundWidth, #1
+    ldr backgroundHeight, [r0, #4]
+    sub backgroundHeight, backgroundHeight, #1
+    
+    // Determining the direction of the ball
+    mov     directionX, #1      // Will get rid of this after we pass it in as arguments, just a quick hack for now
+    mov     directionY, #1      // Will get rid of this after we pass it in as arguments, just a quick hack for now
+    cmp     xCoord, #0
+    moveq   directionX, #1
+    cmp     xCoord, backgroundWidth
+    moveq   directionX, #-1
+    cmp     yCoord, #0
+    moveq   directionY, #1
+    cmp     yCoord, backgroundHeight
+    moveq   directionY, #-1
+    
+    // Drawing the ball everyone 1000 microseconds
+    
+    mov r0, #1000
+    bl  delayMicroseconds
+    
+    ldr r0, =ballImage
+    mla r1, directionX, move, xCoord
+    mla r2, directionY, move, yCoord
+    bl  drawImage
+    
+    mov r0, xCoord      // Returning X and Y coordinates
+    mov r1, yCoord
+    
+    // Clearing aliases
+    .unreq  xCoord
+    .unreq  yCoord
+    .unreq  backgroundWidth
+    .unreq  backgroundHeight
+    .unreq  directionX
+    .unreq  directionY
+    .unreq  move
+    
+    // Initializing downward movement
+    pop     {r4 - r10, fp, lr}
+    bx  lr
 
-
-right:
-	cmp	r4, r6
-	beq	left
-	mov	r0, #10000
-	bl	delayMicroseconds
-	sub	r4, #1
-	ldr	r0, =ballImage
-	mov	r1, r4
-	mov	r2, #516
-	bl	drawImage
-	b	right
-	
-	
-	pop	{r4 - r10, fp, lr}
-	bx	lr
